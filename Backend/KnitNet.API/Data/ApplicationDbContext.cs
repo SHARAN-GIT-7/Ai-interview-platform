64bd3c89233d00ecf1aa0e6a6600808cc0d965b9
Rudhran-001 <rudhran2004@gmail.com>
2026-03-02 00:29:44 +0530

    postgresql setup

---
+++ Backend/KnitNet.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using KnitNet.API.Models;
+
+namespace KnitNet.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
